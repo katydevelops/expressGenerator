893d82b1ec91de8341edbb535434959b70bca41f
Katy Senia <katy.develops@gmail.com>
2022-11-18 14:28:11 -0500

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -3,6 +3,10 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+	firstname: {
+		type: String,
+		default: "",
+	},
 	admin: {
 		type: Boolean,
 		default: false,
--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -8,6 +8,7 @@ campsiteRouter
 	.route("/")
 	.get((req, res, next) => {
 		Campsite.find()
+			.populate("comments.author")
 			.then((campsites) => {
 				res.statusCode = 200;
 				res.setHeader("Content-Type", "application/json");
@@ -18,6 +19,7 @@ campsiteRouter
 	.post(authenticate.verifyUser, (req, res, next) => {
 		Campsite.create(req.body)
 			.then((campsite) => {
+				req.body.author = req.user._id;
 				console.log("Campsite Created ", campsite);
 				res.statusCode = 200;
 				res.setHeader("Content-Type", "application/json");
@@ -43,6 +45,7 @@ campsiteRouter
 	.route("/:campsiteId")
 	.get((req, res, next) => {
 		Campsite.findById(req.params.campsiteId)
+			.populate("comments.author")
 			.then((campsite) => {
 				res.statusCode = 200;
 				res.setHeader("Content-Type", "application/json");
@@ -85,6 +88,7 @@ campsiteRouter
 	.route("/:campsiteId/comments")
 	.get((req, res, next) => {
 		Campsite.findById(req.params.campsiteId)
+			.populate("comments.author")
 			.then((campsite) => {
 				if (campsite) {
 					res.statusCode = 200;
@@ -159,6 +163,7 @@ campsiteRouter
 	.route("/:campsiteId/comments/:commentId")
 	.get((req, res, next) => {
 		Campsite.findById(req.params.campsiteId)
+			.populate("comments.author")
 			.then((campsite) => {
 				if (campsite && campsite.comments.id(req.params.commentId)) {
 					res.statusCode = 200;
